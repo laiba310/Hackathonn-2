f83c28fb371bb37bc90eb0b0fc99b17c75615064
laiba310 <memonlaiba75.com>
2025-01-22 12:20:58 +0500

    first commit

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -8,7 +8,8 @@ import NewSaleServer from "@/components/pge";
 import Rewies from "@/components/rewies";
 
 
-
+export const dynamic ="force-static"
+export const revalidate =60;
 
 export default async function Home() {
 return(
--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,6 +1,5 @@
-import { createClient } from 'next-sanity'
-
-import { apiVersion, dataset, projectId, token } from '../env'
+import { createClient } from 'next-sanity';
+import { apiVersion, dataset, projectId, token } from '../env';
 
 export const client = createClient({
   projectId,
@@ -8,4 +7,9 @@ export const client = createClient({
   apiVersion,
   token,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+  stega: {
+    studioUrl: process.env.NODE_ENV === "production"
+      ? `https://${process.env.VERCEL_URL}/studio` // Deployed environment
+      : 'http://localhost:3000/studio', // Local development fallback
+  },
+});
