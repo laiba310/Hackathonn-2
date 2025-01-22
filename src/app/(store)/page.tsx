ef135f00c57ec0cc50344755acc8452da6eb38c2
laiba310 <memonlaiba75.com>
2025-01-22 11:55:57 +0500

    first commit

---
+++ src/app/(store)/page.tsx
@@ -0,0 +1,25 @@
+
+import BrowseByStyle from "@/components/dressStyle";
+import Hero from "@/components/hero copy";
+import Logos from "@/components/logos";
+
+import NewArrivalsServer from "@/components/page";
+import NewSaleServer from "@/components/pge";
+import Rewies from "@/components/rewies";
+
+
+
+
+export default async function Home() {
+return(
+  <div>
+<Hero />
+<Logos />
+<div>
+< NewArrivalsServer /></div>
+<NewSaleServer />
+<BrowseByStyle/>
+<Rewies />
+ </div>
+
+);}
